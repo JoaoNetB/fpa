ab596281a6e478acc2809aca77b39149daf0ebc0
João Bevolo <netojb156@gmail.com>
2023-10-25 22:40:25 -0300

    Modificação navbar e exclusão de arquivos inúteis
    Este commit modifica o funcionamento da navbar, exclui arquivos
    desnecessários e limpa a formatação do aplicativo.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav v-if="paginaAtual">
+    <router-link to="/">Início</router-link>
+    <button v-on:click="voltar">
+      Voltar
+    </button>
   </nav>
   <router-view/>
 </template>
 
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        paginaAtual: this.$route.name == "inicio" ? false : true 
+      }
+    },
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  * {
+    padding: 0;
+    border: 0;
+    margin: 0;
+    background-color: #00171F;
+  }
 </style>
